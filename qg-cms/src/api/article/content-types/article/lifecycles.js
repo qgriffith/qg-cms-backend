787e0b7ddd83f38282288c41218dd5b867896d5d
Quenten Griffith <qgriffith@gmail.com>
2022-07-19 09:08:35 -0400

    automate the slug field

---
+++ qg-cms/src/api/article/content-types/article/lifecycles.js
@@ -0,0 +1,16 @@
+const slugify = require("slugify");
+
+module.exports = {
+  beforeCreate(event) {
+    const { data } = event.params;
+    if (data.title) {
+      data.slug = slugify(data.title, { lower: true });
+    }
+  },
+  beforeUpdate(event) {
+    const { data } = event.params;
+    if (data.title) {
+      data.slug = slugify(data.title, { lower: true });
+    }
+  },
+}
