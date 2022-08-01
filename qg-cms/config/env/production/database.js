7107803cb4834884984f35453e9a76944cde5476
Quenten Griffith <qgriffith@gmail.com>
2022-08-01 14:25:36 -0400

    fixing db timeout issue https://github.com/strapi/strapi/issues/8117

--- qg-cms/config/env/production/database.js
+++ qg-cms/config/env/production/database.js
@@ -15,5 +15,15 @@ module.exports = ({ env }) => {
       },
       debug: false,
     },
+    acquireConnectionTimeout: 7000,
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 8000,
+      acquireTimeoutMillis: 8000,
+      idleTimeoutMillis: 8000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    }
   };
 };
